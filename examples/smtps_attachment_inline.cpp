3a44ef8554d1ebc871b0bf9f49ad93e0c11429eb
Hannah Sauermann <hannah.sauermann@seclous.com>
2022-04-20 13:24:01 +0200

    Added example for sending inline attachments.
    Also fixed smtps attachment example.

--- examples/smtps_attachment_inline.cpp
+++ examples/smtps_attachment_inline.cpp
@@ -1,9 +1,9 @@
 /*
 
-smtps_attachment.cpp
+smtps_attachment_inline.cpp
 --------------------
  
-Connects to SMTP server via SSL and sends a message with attached files.
+Connects to SMTP server via SSL and sends a message with inline attachments.
 
  
 Copyright (C) 2016, Tomislav Karastojkovic (http://www.alepho.com).
@@ -44,14 +44,16 @@ int main()
         msg.from(mail_address("mailio library", "mailio@gmail.com"));// set the correct sender name and address
         msg.add_recipient(mail_address("mailio library", "mailio@gmail.com"));// set the correct recipent name and address
         msg.subject("smtps message with attachment");
-        msg.content("Here are Aleph0 and Infinity pictures.");
+        msg.content("Here are Aleph0 <img src=\"cid:img0\" /> and Infinity <img src=\"cid:img1\" /> pictures.");
+        msg.content_type(mailio::mime::media_type_t::TEXT, "html");
+        msg.content_transfer_encoding(mailio::mime::content_transfer_encoding_t::QUOTED_PRINTABLE);
 
         ifstream ifs1("aleph0.png", std::ios::binary);
         ifstream ifs2("infinity.png", std::ios::binary);
-        list<tuple<std::istream&, string, message::content_type_t>> atts;
-        atts.push_back(make_tuple(std::ref(ifs1), string("aleph0.png"), message::content_type_t(message::media_type_t::IMAGE, "png")));
-        atts.push_back(make_tuple(std::ref(ifs2), "infinity.png", message::content_type_t(message::media_type_t::IMAGE, "png")));
-        msg.attach(atts);
+        list<tuple<std::istream&, string, message::content_type_t, string>> atts;
+        atts.push_back(make_tuple(std::ref(ifs1), string("aleph0.png"), message::content_type_t(message::media_type_t::IMAGE, "png"), "img0"));
+        atts.push_back(make_tuple(std::ref(ifs2), "infinity.png", message::content_type_t(message::media_type_t::IMAGE, "png"), "img1"));
+        msg.attach_inline(atts);
 
         // use a server with plain (non-SSL) connectivity
         smtps conn("smtp.mailserver.com", 465);
